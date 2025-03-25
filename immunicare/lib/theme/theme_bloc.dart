2e59ec4b592aa2f7f82c4e177513f62d6c0c3c3c
Chol Daniel Deng Dau <144565388+Dau2004@users.noreply.github.com>
2025-03-25 19:30:57 +0200

    Updated theme_bloc.dart

--- immunicare/lib/theme/theme_bloc.dart
+++ immunicare/lib/theme/theme_bloc.dart
@@ -4,6 +4,7 @@ import 'package:immunicare/theme/theme_state.dart';
 import 'package:immunicare/theme/app_themes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+// Bloc responsible for managing the application's theme.
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
   ThemeBloc() : super(ThemeState.initial()) {
     on<InitializeThemeEvent>(_onInitializeTheme);
@@ -12,28 +13,33 @@ class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
     // Initialize theme on creation
     add(InitializeThemeEvent());
   }
-
+  // Handler for initializing the theme.
   Future<void> _onInitializeTheme(
     InitializeThemeEvent event,
     Emitter<ThemeState> emit,
   ) async {
+    // Fetch the shared preferences instance.
     final prefs = await SharedPreferences.getInstance();
+    // Retrieve the theme mode from preferences, defaulting to false (light mode) if not set.
     final isDarkMode = prefs.getBool('isDarkMode') ?? false;
-
+     // Emit the theme state based on the retrieved preference.
     emit(ThemeState(
       isDarkMode: isDarkMode,
       themeData: isDarkMode ? AppThemes.darkTheme : AppThemes.lightTheme,
     ));
   }
-
+// Handler for toggling the theme.
   Future<void> _onToggleTheme(
     ToggleThemeEvent event,
     Emitter<ThemeState> emit,
   ) async {
+    // Toggle the current theme mode.
     final newIsDarkMode = !state.isDarkMode;
+    // Fetch the shared preferences instance.
     final prefs = await SharedPreferences.getInstance();
+    // Save the new theme mode in preferences.
     await prefs.setBool('isDarkMode', newIsDarkMode);
-
+    // Emit the new theme state based on the toggled preference.
     emit(ThemeState(
       isDarkMode: newIsDarkMode,
       themeData: newIsDarkMode ? AppThemes.darkTheme : AppThemes.lightTheme,
